6aaa13c44031b3674393e87abf590c9ff787aa34
vlxu_git <vallens.kho@gmail.com>
2023-04-23 15:02:41 -0400

    Implemented InvalidGuessException

---
+++ src/core_exceptions/InvalidGuessException.java
@@ -0,0 +1,7 @@
+package src.core_exceptions;
+
+public class InvalidGuessException extends Exception {
+    public InvalidGuessException() {
+        super("Invalid guess: a guess should be a single alphabet [a-zA-Z]");
+    }
+}
