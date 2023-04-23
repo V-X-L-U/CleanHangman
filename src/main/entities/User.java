e78425621df94b8cddd4f049e70171ad4baa8a7c
vlxu_git <vallens.kho@gmail.com>
2023-04-23 14:34:49 -0400

    Implemented User entity

---
+++ src/main/entities/User.java
@@ -0,0 +1,36 @@
+package src.main.entities;
+
+public class User {
+    private String userName;
+    private int numSuccess;
+
+    public User(String userName) {
+        numSuccess = 0;
+    }
+
+    /**
+     * Returns this user's name.
+     *
+     * @return this user's name.
+     * */
+    String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Returns how many words this user has successfully guessed since account creation.
+     *
+     * @return this user's number of successful games.
+     * */
+    int getNumSuccess() {
+        return numSuccess;
+    }
+
+    /**
+     * Records that the user has successfully guessed another word.
+     * */
+    void incNumSuccess() {
+        numSuccess += 1;
+    }
+}
+
